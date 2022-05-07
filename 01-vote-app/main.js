69ecf9f959afe56273abfc603760151466d009b3
SureshKuchana <kuchana123.suresh@gmail.com>
2022-05-07 22:16:45 +0530

    restructured code for component

--- 01-vote-app/main.js
+++ 01-vote-app/main.js
@@ -1,3 +1,48 @@
+const submissionComponent = {
+  template: `<div style="display: flex; width: 100%">
+      <figure class="media-left">
+        <img class="image is-64x64"
+          :src="submission.submissionImage">
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong>
+              <a :href="submission.url" class="has-text-info">
+                {{ submission.title }}
+              </a>
+              <span class="tag is-small">#{{ submission.id }}</span>
+            </strong>
+            <br>
+              {{ submission.description }}
+            <br>
+            <small class="is-size-7">
+              Submitted by:
+              <img class="image is-24x24"
+                :src="submission.avatar">
+            </small>
+          </p>
+        </div>
+      </div>
+      <div class="media-right">
+        <span class="icon is-small" @click="upVote(submission.id)">
+          <i class="fa fa-chevron-up"></i>
+          <strong class="has-text-info">{{ submission.votes }}</strong>
+        </span>
+      </div>
+    </div>`,
+  props: ["submission", "submissions"],
+  methods: {
+    upVote(submissionId) {
+      const submission = this.submissions.find(
+        (submission) => submission.id === submissionId
+      );
+
+      submission.votes++;
+    },
+  },
+};
+
 const upVoteApp = {
   data() {
     return { submissions: Seed.submissions };
@@ -9,14 +54,8 @@ const upVoteApp = {
       });
     },
   },
-  methods: {
-    upVote(submissionId) {
-      const submission = this.submissions.find(
-        (submission) => submission.id === submissionId
-      );
-
-      submission.votes++;
-    },
+  components: {
+    "submission-component": submissionComponent,
   },
 };
 
