0965d7520f63e57af0467666e5e308253e08fa67
SureshKuchana <kuchana123.suresh@gmail.com>
2024-05-06 21:19:28 +0530

    ponyracer setup

---
+++ ponyracer/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="container" style="margin-top: 70px">
+    <h1>You did it!</h1>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+
+  components: {}
+})
+</script>
+
+<style></style>
